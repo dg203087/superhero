5728e6c51e298ba4c9a84bcca5b7dc38e90e1194
github username <github email address>
2019-11-20 13:13:02 -0500

    Initial Commit

---
+++ spec/superhero_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Superhero do
+  it "has a version number" do
+    expect(Superhero::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
