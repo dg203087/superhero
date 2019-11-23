306797f26c70ef5a39b935b1d13d8e75e11bdaa1
github username <github email address>
2019-11-23 18:15:27 -0500

    building out menu class

--- lib/superhero/api.rb
+++ lib/superhero/api.rb
@@ -1,9 +1,13 @@
-# class Superhero::API
-#     BASE_URL = https://superheroapi.com/API
-#     API_KEY = 814865975490
-#     #should be saved in an environment and not hard coded
+class Superhero::API
+    BASE_URL = "https://superheroapi.com/api.php/"
+    ACCESS_TOKEN = "814865975490"
+    #should be saved in an environment and not hard coded
 
-#     def self.query_superhero_db
-#         results = RestClient.get("#{BASE_URL}/#{API_KEY}//character-id")
-#     end
-# end
+    def self.query_superhero_db(input)
+        # input = gets.strip
+        # input = input.downcase.tr(" ","_") #make URL friendly parameterize
+        results = HTTParty.get("#{BASE_URL}#{ACCESS_TOKEN}/search/#{input}")
+
+        Superhero::Hero.new(results)
+    end
+end
