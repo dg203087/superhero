ff5ae8b672a7b9bc8358f818a76260c4a0d6a25b
github username <github email address>
2019-11-25 23:32:35 -0500

    flesh out the list superheroes method

--- lib/superhero/hero.rb
+++ lib/superhero/hero.rb
@@ -1,60 +1,42 @@
 class Superhero::Hero
    
-    attr_accessor :id, :full_name, :place_of_birth, :alignment, :publisher, :race 
+    attr_accessor :full_name, :race, :alignment, :home_base, :publisher, :intelligence, 
+    :strength, :speed, :durability, :power, :combat
 
     @@all = []
 
-    def initialize(args={})
-    binding.pry
-        args.each do |attribute, value|
-            value.each do |attr, val|
-                
-                if attr == :id
-                    puts "#{id}" 
-                end
-            end
-        end
+    def initialize(full_name, race, alignment, home_base, publisher, intelligence, 
+        strength, speed, durability, power, combat)
+        @full_name = full_name
+        @race = race
+        @alignment = alignment
+        @home_base = home_base
+        @publisher = publisher
+        @intelligence = intelligence
+        @strength = strength
+        @speed = speed
+        @durability = durability
+        @power = power
+        @combat = combat
         @@all << self
     end
 
-    def self.shazam_info
-        #should return a bunch of instances of HEROES
-        
+    def self.all
+        @@all
+    end
 
-        # {"id":"69",
-        # "name":"Batman",
-        # "powerstats":{"intelligence":"81",
-        # "strength":"40",
-        # "speed":"29",
-        # "durability":"55",
-        # "power":"63",
-        # "combat":"90"},
+    # def valid?
 
-        # "biography":{"full-name":"Terry McGinnis",
-        # "alter-egos":"No alter egos found.",
-        # "aliases":["Batman II","The Tomorrow Knight","The second Dark Knight","The Dark Knight of Tomorrow","Batman Beyond"],
-        # "place-of-birth":"Gotham City, 25th Century",
-        # "first-appearance":"Batman Beyond #1",
-        # "publisher":"DC Comics",
-        # "alignment":"good"},
-        
-        # "appearance":{"gender":"Male",
-        # "race":"Human",
-        # "height":["5'10","178 cm"],
-        # "weight":["170 lb","77 kg"],
-        # "eye-color":"Blue",
-        # "hair-color":"Black"},
-        
-    
+    # end
 
-
-
+    def self.find_by_index(index)
+        @@all[index]
     end
 
     def self.list_superheroes(letter)
         puts <<-DOC
         This is where I will list all superheroes by a certain letter
-    DOC
+        DOC
     end
 
 end
