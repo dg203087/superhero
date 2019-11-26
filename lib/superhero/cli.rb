a7809d9d086e50be04e935c1c9efb04e9346a4fd
github username <github email address>
2019-11-25 23:31:24 -0500

    if statement troubleshooting

--- lib/superhero/cli.rb
+++ lib/superhero/cli.rb
@@ -5,34 +5,59 @@ class Superhero::CLI
         puts "Welcome to Superhero Finder!"
         puts "*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*"
         menu
-        goodbye
     end
 
     def menu
-        puts "Enter the name of the superhero you'd like to learn about." 
+        puts "Enter the name of the superhero you'd like to learn about:" 
         puts "Type exit to leave."
         input = gets.strip
             
-        if input != nil #find a match in the API
+        if input == "exit"
+            goodbye
+        elsif input != nil         #.valid? == true #find a match in the API
             Superhero::API.query_superhero_db(input) 
-            @attribute = Superhero::Hero.find_by_index(input.to_i - 1)
+            @attribute = Superhero::Hero.find_by_index(input.to_i - 1) 
+            #instance variable set equal all initialized objects to be called below
             display_info
-        elsif input == "list"
-                list_superheroes_by_letter
+
+            puts "Would you like to learn about another superhero? y/n"
+            input = gets.strip.downcase
+            if input == "y"
+                menu
+            elsif input == "n"
+                goodbye
+            else
+                puts ""
+                puts "Not sure what you're trying to say, let's try again."
+                puts ""
+                menu
+            end
+        # elsif input == "list"
+        #     list_superheroes_by_letter
         else 
-                puts "I don't know that superhero, try again."
-                puts "Type exit to leave."
+            puts "I don't know that superhero, try again."
+            puts "Type exit to leave."
         end
     end
 
     def display_info 
-        puts ""
-        puts "----------- Profile -----------"
+        puts "------------- Profile -------------"
         puts ""
         puts "Full Name:           #{@attribute.full_name}"
         puts "Race:                #{@attribute.race}"
         puts "Alignment:           #{@attribute.alignment}"
-        puts "Home Base:           #{@attribute.home_base}"   
+        puts "Home Base:           #{@attribute.home_base}"
+        puts "Publisher:           #{@attribute.publisher}"
+        puts ""
+        puts "----------- Power Stats -----------"
+        puts ""
+        puts "Intelligence:        #{@attribute.intelligence}"
+        puts "Strength:            #{@attribute.strength}"
+        puts "Speed:               #{@attribute.speed}"
+        puts "Durability:          #{@attribute.durability}"
+        puts "Power:               #{@attribute.power}"
+        puts "Combat:              #{@attribute.combat}"
+        puts ""
     end
 
     def list_superheroes_by_letter(letter)
@@ -47,6 +72,7 @@ class Superhero::CLI
     end
 
     def goodbye
-        puts "See you soon Superhero!"
+        puts ""
+        puts "Off to another adventure! See you soon Superhero!"
     end
 end
