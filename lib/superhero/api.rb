961c02f281f7cf9010ba48109a59385d58f0506f
github username <github email address>
2019-11-25 23:33:42 -0500

    works effectively, need to take out hard coding

--- lib/superhero/api.rb
+++ lib/superhero/api.rb
@@ -4,10 +4,23 @@ class Superhero::API
     #should be saved in an environment and not hard coded
 
     def self.query_superhero_db(input)
-        # input = gets.strip
-        # input = input.downcase.tr(" ","_") #make URL friendly parameterize
+        input = input.downcase.tr(" ","_") #make URL friendly parameterize
         results = HTTParty.get("#{BASE_URL}#{ACCESS_TOKEN}/search/#{input}")
-
-        Superhero::Hero.new(results)
+        
+        results["results"].each do |attribute|
+            full_name = attribute["biography"]["full-name"]
+            race = attribute["appearance"]["race"]
+            alignment = attribute["biography"]["alignment"]
+            home_base = attribute["work"]["base"]
+            publisher = attribute["biography"]["publisher"]
+            intelligence = attribute["powerstats"]["intelligence"]
+            strength = attribute["powerstats"]["strength"]
+            speed = attribute["powerstats"]["speed"]
+            durability = attribute["powerstats"]["durability"]
+            power = attribute["powerstats"]["power"]
+            combat = attribute["powerstats"]["combat"]
+            Superhero::Hero.new(full_name, race, alignment, home_base, publisher, intelligence, 
+            strength, speed, durability, power, combat)
+        end  
     end
 end
