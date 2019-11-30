be39564c9e3ba291f7332aa21c058c69a642310a
github username <github email address>
2019-11-30 14:02:55 -0500

    create .each loop for popular superheroes

--- lib/superhero/api.rb
+++ lib/superhero/api.rb
@@ -9,29 +9,48 @@ class Superhero::API
         "Iron_Man", "Jessica_Jones", "Nebula", "Night_Crawler", "Quicksilver", "Robin", 
         "Rogue", "Spider Man", "Superman", "Valkyrie", "Winter_Soldier", "Wonder_Woman", 
         "Wolverine", "Yoda"]
-        popular_names.each do |hero|
-            puts hero.gsub('_', ' ')
+
+        popular_names.each do |hero_name|
+            results = HTTParty.get("#{BASE_URL}#{ACCESS_TOKEN}/search/#{hero_name}")
+
+            results["results"].each do |attribute|
+                name = ["name"]
+                full_name = attribute["biography"]["full-name"]
+                race = attribute["appearance"]["race"]
+                alignment = attribute["biography"]["alignment"]
+                home_base = attribute["work"]["base"]
+                publisher = attribute["biography"]["publisher"]
+                intelligence = attribute["powerstats"]["intelligence"]
+                strength = attribute["powerstats"]["strength"]
+                speed = attribute["powerstats"]["speed"]
+                durability = attribute["powerstats"]["durability"]
+                power = attribute["powerstats"]["power"]
+                combat = attribute["powerstats"]["combat"]
+                Superhero::Hero.new(name, full_name, race, alignment, home_base, publisher, intelligence, 
+                strength, speed, durability, power, combat)
+            end  
         end
     end
 
-    def self.query_superhero_db(input)
-        input = input.downcase.tr(" ","_") #make URL friendly parameterize
-        results = HTTParty.get("#{BASE_URL}#{ACCESS_TOKEN}/search/#{input}")
+    # def self.query_superhero_db(input)
+    #     input = input.downcase.tr(" ","_") #make URL friendly parameterize
+    #     results = HTTParty.get("#{BASE_URL}#{ACCESS_TOKEN}/search/#{input}")
         
-        results["results"].each do |attribute|
-            full_name = attribute["biography"]["full-name"]
-            race = attribute["appearance"]["race"]
-            alignment = attribute["biography"]["alignment"]
-            home_base = attribute["work"]["base"]
-            publisher = attribute["biography"]["publisher"]
-            intelligence = attribute["powerstats"]["intelligence"]
-            strength = attribute["powerstats"]["strength"]
-            speed = attribute["powerstats"]["speed"]
-            durability = attribute["powerstats"]["durability"]
-            power = attribute["powerstats"]["power"]
-            combat = attribute["powerstats"]["combat"]
-            Superhero::Hero.new(full_name, race, alignment, home_base, publisher, intelligence, 
-            strength, speed, durability, power, combat)
-        end  
-    end
+    #     results["results"].each do |attribute|
+    #         name = ["name"]
+    #         full_name = attribute["biography"]["full-name"]
+    #         race = attribute["appearance"]["race"]
+    #         alignment = attribute["biography"]["alignment"]
+    #         home_base = attribute["work"]["base"]
+    #         publisher = attribute["biography"]["publisher"]
+    #         intelligence = attribute["powerstats"]["intelligence"]
+    #         strength = attribute["powerstats"]["strength"]
+    #         speed = attribute["powerstats"]["speed"]
+    #         durability = attribute["powerstats"]["durability"]
+    #         power = attribute["powerstats"]["power"]
+    #         combat = attribute["powerstats"]["combat"]
+    #         Superhero::Hero.new(hero_name, full_name, race, alignment, home_base, publisher, intelligence, 
+    #         strength, speed, durability, power, combat)
+    #     end  
+    # end
 end
