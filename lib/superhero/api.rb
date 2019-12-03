0052f45047734f41042ec2497fab0cad763a8469
github username <github email address>
2019-12-02 21:44:44 -0500

    change popular names array to id numbers not str

--- lib/superhero/api.rb
+++ lib/superhero/api.rb
@@ -3,18 +3,21 @@ class Superhero::API
     ACCESS_TOKEN = "814865975490"
     #should be saved in an environment and not hard coded
 
-    def popular_superheroes
-        popular_names = ["Batman", "Batwoman", "Beast", "Black_Widow", "Captain_America", 
-        "Captain_Marvel", "Catwoman", "Deadpool", "Flash", "Harley_Quinn", "Hawkeye", 
-        "Iron_Man", "Jessica_Jones", "Nebula", "Night_Crawler", "Quicksilver", "Robin", 
-        "Rogue", "Spider Man", "Superman", "Valkyrie", "Winter_Soldier", "Wonder_Woman", 
-        "Wolverine", "Yoda"]
+    def self.create_popular_superheroes
+        popular_names = [70, 73, 75, 107, 149, 156, 165, 213, 263, 309, 313, 346, 361, 
+        487, 490, 536, 561, 567, 621, 644, 684, 714, 720, 717, 729]
+        # popular_names = ["Batman", "Batwoman", "Beast", "Black_Widow", "Captain_America", 
+        # "Captain_Marvel", "Catwoman", "Deadpool", "Flash", "Harley_Quinn", "Hawkeye", 
+        # "Iron_Man", "Jessica_Jones", "Nebula", "Quicksilver", "Robin", 
+        # "Rogue", "Spider-Man", "Superman", "Valkyrie", "Winter_Soldier", "Wonder_Woman", 
+        # "Wolverine", "Yoda"]
 
-        popular_names.each do |hero_name|
-            results = HTTParty.get("#{BASE_URL}#{ACCESS_TOKEN}/search/#{hero_name}")
-
+        popular_names.each do |hero_id|
+            results = HTTParty.get("#{BASE_URL}#{ACCESS_TOKEN}/#{hero_id}")
+            # if results.has_key?()
+    
             results["results"].each do |attribute|
-                name = ["name"]
+                name = attribute["name"]
                 full_name = attribute["biography"]["full-name"]
                 race = attribute["appearance"]["race"]
                 alignment = attribute["biography"]["alignment"]
@@ -26,10 +29,14 @@ class Superhero::API
                 durability = attribute["powerstats"]["durability"]
                 power = attribute["powerstats"]["power"]
                 combat = attribute["powerstats"]["combat"]
+            
                 Superhero::Hero.new(name, full_name, race, alignment, home_base, publisher, intelligence, 
                 strength, speed, durability, power, combat)
-            end  
-        end
+            end
+        # else 
+        #     binding.pry
+        # end 
+        end 
     end
 
     # def self.query_superhero_db(input)
